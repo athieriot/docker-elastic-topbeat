3de39d06a76088c46b5aa7d97851659e893996c7
Aurélien Thieriot <a.thieriot@gmail.com>
2016-03-30 22:16:05 +0100

    Support Topbeat 1.2
    - Drop manual Environment variables replacement in favour of built in
      one
    - If Elasticsearch Profile, use built in template management. Business
      as usual for other use cases

--- files/start.sh
+++ files/start.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-OUTPUT=${PROFILE:-elasticsearch}
-
 mkdir -p /topbeat/config && mkdir -p /topbeat/data
 
-if [ $OUTPUT == "logstash" ] && [ -z PORT ]; then
-  PORT=5044
+if [ "$PROFILE" != "custom" ]; then
+  cp /topbeat/topbeat.${PROFILE:-elasticsearch}.yml /topbeat/config/topbeat.yml
 fi
 
-if [ $OUTPUT != "custom" ]; then
-  cp /topbeat/topbeat.${OUTPUT}.yml /topbeat/config/topbeat.yml
-  sed -i "s#{CPU_PER_CORE}#${CPU_PER_CORE:-false}#g" /topbeat/config/topbeat.yml
-  sed -i "s#{PERIOD}#${PERIOD:-10}#g" /topbeat/config/topbeat.yml
-  sed -i "s#{PROCS}#${PROCS:-.*}#g" /topbeat/config/topbeat.yml
-  sed -i "s#{INDEX}#${INDEX:-topbeat}#g" /topbeat/config/topbeat.yml
-  sed -i "s#{HOST}#${HOST:-${OUTPUT}}#g" /topbeat/config/topbeat.yml
-  sed -i "s#{PORT}#${PORT:-9200}#g" /topbeat/config/topbeat.yml
-  sed -i "s#{SHIPPER_NAME}#${SHIPPER_NAME}#g" /topbeat/config/topbeat.yml
-  sed -i "s#{SHIPPER_TAGS}#${SHIPPER_TAGS}#g" /topbeat/config/topbeat.yml
-fi
-
-if [ $OUTPUT == "elasticsearch" ] || [ $EXTERNAL_ELASTIC_HOST ]; then
+if [ "$PROFILE" != "elasticsearch" ] && [ $EXTERNAL_ELASTIC_HOST ]; then
   if [ -z $DRY_RUN ]; then
     # wait for elasticsearch to start up
     ELASTIC_PATH=${EXTERNAL_ELASTIC_HOST:-${HOST:-elasticsearch}}:${EXTERNAL_ELASTIC_PORT:-${PORT:-9200}}
